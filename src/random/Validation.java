824d9ffe558f9b3d9f9d127b086cf82aa9870596
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-06-21 13:26:43 -0700

    DiceRoll, RockPaperScissors, IntroToRandom, Validation ip

--- src/random/Validation.java
+++ src/random/Validation.java
@@ -21,9 +21,26 @@ public class Validation {
 		System.out.println(randomNumber);
 
 		// 3. Use each value of randomNumber to give the user a random compliment.
+if(randomNumber==2) {
+	JOptionPane.showMessageDialog(null, "Nice shoes!");	
+}
+
+if(randomNumber==4) {
+	JOptionPane.showMessageDialog(null, "Nice hair!");	
+}
+
+if(randomNumber==3) {
+	JOptionPane.showMessageDialog(null, "Nice shirt!");	
+}
+
+if(randomNumber==0) {
+	JOptionPane.showMessageDialog(null, "Your very nice!");	
+}
 
 		// 4. Repeat all the code above 10 times
 		
 		// 5. Find someone to test out your program. They will like it :)
+		
+		//6.2,4,3,0
 	}
 }
